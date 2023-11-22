870b1bbc87c03f5d44a6fe674b16cad4f0c25448
Chooooooo <suntail2002@naver.com>
2023-11-22 22:28:02 +0900

    Feat: 맵 클릭 이벤트 추가
    - 맵 클릭 이벤트 추가
    
    Related to #2

--- mern_client/src/components/MapContainer.tsx
+++ mern_client/src/components/MapContainer.tsx
@@ -8,6 +8,8 @@ function MapContainer() {
 
   const initMap = (map: naver.maps.Map) => {
     setMap(map);
+    naver.maps.Event.addListener(map, "click", () => {
+    });
   };
 
   return <Map width="100%" height="100%" initMap={initMap} />;
